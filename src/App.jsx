60e15ad886706e1015f512566e9a6d78d3f9b89d
Francisco <ftarantuviez.dev@gmail.com>
2020-07-18 20:48:32 -0300

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Row} from './components/Row/Row'
+import requests from './Requests'
+
 
 class App extends React.Component{
+
     render(){
         return(
             <>
-                <Row title="Netflix Originals" />
+                <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+                <Row title="Trending now" fetchUrl={requests.fetchTrending} />
             </>
         )
     } 
--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from '../../axios'
+export const Row = ({title, fetchUrl}) => {
 
-export const Row = ({title}) => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request;
+        }
+
+        fetchData()
+
+    }, [fetchUrl])
+
     return(
-        /* title */
-        <h1>{title}</h1>
+        <>
+        
+        {/* title */}
+            <h1>{title}</h1>
 
-        /* container -> posters */
-    )
+        {/* container -> posters */ }
+            <ul>
+                {movies.map(movie => (
+                    <li>
+
+                    </li>
+                ))}
+            </ul>
+        </>
+
+)
 }
