6b763f4190a75cbdadf82969c2a5ce9e526cc30b
francisco <ftarantuviez.dev@gmail.com>
2020-07-18 16:04:48 -0300

    lets start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//2f07a1f435aaa814c19fa7ab7767bcf0
+class App extends React.Component{
+    render(){
+        return(
+            <h1>App</h1>
+        )
+    } 
+}
+
+export default App;
